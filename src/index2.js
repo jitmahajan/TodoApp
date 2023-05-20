9a49c533f6f43dd3c2f36c19d17de35496ac3c63
Jitesh Mahajan <jiteshmahajan22@gmail.com>
2023-05-20 20:46:00 +0530

    express_use

--- src/index2.js
+++ src/index2.js
@@ -1,5 +1,10 @@
-import {insert} from "./index.js"
+import express from "express";
 
+ const app = express();
 
-let data = { message : "Hi!!!"};
-insert(data);
+ //127.0.0.1:4400
+app.get("/", (req, res) => {
+    res.json({message:"hi...."});
+});
+
+ app.listen(4400);
